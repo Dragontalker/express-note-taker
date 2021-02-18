3852e0f6ef43684c3551ed7ff36ef788b0ff96fa
Richard <richard.yang.tong@gmail.com>
2021-02-18 10:31:51 -0500

    Changed the post handler that it now reads property from body of request.

--- middleware/api.js
+++ middleware/api.js
@@ -6,8 +6,8 @@ const data = require('../db/db.json');
 router.get('/', (req, res) => res.json(data));
 
 router.post('/', (req, res) => {
-    const newTitle = req.params.title;
-    const newText = req.params.text;
+    const newTitle = req.body.title;
+    const newText = req.body.text;
     if (!newTitle || !newText) {
         res.status(400).send({msg: 'Need non-empty title and text input.'})
     };
