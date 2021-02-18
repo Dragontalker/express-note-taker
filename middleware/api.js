eeed8472ac036e574a718867c93461a504595dd8
Richard <richard.yang.tong@gmail.com>
2021-02-18 10:29:53 -0500

    Added a 400 bad request when title or text is missing from request.

--- middleware/api.js
+++ middleware/api.js
@@ -6,7 +6,11 @@ const data = require('../db/db.json');
 router.get('/', (req, res) => res.json(data));
 
 router.post('/', (req, res) => {
-    
+    const newTitle = req.params.title;
+    const newText = req.params.text;
+    if (!newTitle || !newText) {
+        res.status(400).send({msg: 'Need non-empty title and text input.'})
+    };
 });
 
 module.exports = router;
