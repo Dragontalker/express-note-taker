317bc62edfef571f0d8dc2c395345a3a30e5625a
Richard <richard.yang.tong@gmail.com>
2021-02-19 14:56:15 -0500

    Removed the static file middleware since we already have a middleware for html files.

--- server.js
+++ server.js
@@ -4,7 +4,7 @@ const path = require('path');
 const app = express();
 
 app.use(express.json());
-app.use(express.static(path.join(__dirname, 'public')));
+
 
 app.use('/api/notes', require('./middleware/api'));
 app.use('/', require('./middleware/html'));
