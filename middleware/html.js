b0eb54eae89e10b17c7ff3afb54480059b0303fa
Richard <richard.yang.tong@gmail.com>
2021-02-19 14:50:16 -0500

    Created a helper function for making the path.

--- middleware/html.js
+++ middleware/html.js
@@ -4,5 +4,12 @@ const path = require('path');
 const express = require('express');
 const router = express.Router();
 
+const getPath = (name) => {
+    return path.join(__dirname, '..', 'public', `${name}.html`);
+};
 
+// Test 1
+console.log(getPath("notes"));
 
+// Test 2
+console.log(getPath("index"));
