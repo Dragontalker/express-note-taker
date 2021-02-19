c3bd9890853fe55a5b01e60bac6786f6b96bb2e6
Richard <richard.yang.tong@gmail.com>
2021-02-19 17:51:02 -0500

    Added comments for server.js.

--- server.js
+++ server.js
@@ -1,14 +1,23 @@
+// dependencies
 const express = require('express');
 const path = require('path');
 
+// load express.js
 const app = express();
 
+// mount middleware for json
 app.use(express.json());
+
+// mount middleware for static files
 app.use(express.static(path.join(__dirname, 'public')));
 
+// mount middleware for api router
 app.use('/api/notes', require('./middleware/api'));
+
+// mount middleware for html router
 app.use('/', require('./middleware/html'));
 
+// start server
 const PORT = process.env.PORT || 3000;
 
 app.listen(PORT, () => {
